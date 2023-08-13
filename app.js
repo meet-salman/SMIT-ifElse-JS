a7b511d996d68960eac6b118ba1cf08af56e1fdb
Salman Ahmed <say2salmanahmed@gmail.com>
2023-08-13 20:04:36 +0500

    If Else Statement

---
+++ app.js
@@ -0,0 +1,56 @@
+// var checkNum = 10 > 8 || 8 > 10 && 15 < 10;
+// console.log(checkNum);
+
+
+
+
+// var num = 10;
+
+// if (num > 20 || num < 5 && num > 5) {
+//     console.log("If Chal Raha Hy");
+
+//     if (num < 11) {
+//         console.log("If Ka If Chal Raha Hy");
+//     }
+//     else {
+//         console.log("If Ke Andar Wale If Ka Else");
+//     }
+// }
+
+// else {
+//     console.log("Else Chal Raha Hy");
+
+//     if (num < 15) {
+//         console.log("Else Ka If Chal Raha Hy");
+//     }
+//     else {
+//         console.log("Else Ke If Ka Else");
+//     }
+// }
+
+
+// let trues = 10;
+// let falses = 20;
+
+// if (trues > falses || falses < trues || trues < falses) {
+//     console.log("If chal rha hy");
+    
+// } else {
+//     console.log("Khatam...Tata Bye bye");
+// }
+
+
+// let names = prompt("Name batao");
+// let age = +prompt("Ktny saal ke ho?");
+// let job = prompt("Job is Government OR Private?");
+// let salary = +prompt("Kamaty ktna ho?");
+// let bankBalance = +prompt("Bank Balance hy? ktna hy?");
+// let car = +prompt("Car kon c hy?");
+// let habbit = prompt("Nasha to nhi krty?");
+
+// if (age >= 18 && age <= 21 && job === "Government" && salary >= 85000 && bankBalance >= 3000000 && car === "Civic" && habbit === "No") {
+//     console.log("Larka Acha hy.");
+
+// } else {
+//     console.log("Samajh mn nhi aaya.");
+// }
