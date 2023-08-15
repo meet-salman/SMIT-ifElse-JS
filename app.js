7c49c65cd3122062f3634008bc3f8a8f8a609935
Salman Ahmed <say2salmanahmed@gmail.com>
2023-08-15 12:33:54 +0500

    If Else

--- app.js
+++ app.js
@@ -29,15 +29,17 @@
 // }
 
 
+
 // let trues = 10;
 // let falses = 20;
 
 // if (trues > falses || falses < trues || trues < falses) {
 //     console.log("If chal rha hy");
-    
+
 // } else {
 //     console.log("Khatam...Tata Bye bye");
 // }
+
 
 
 // let names = prompt("Name batao");
@@ -54,3 +56,114 @@
 // } else {
 //     console.log("Samajh mn nhi aaya.");
 // }
+
+
+
+
+// let hindsa = 27;
+
+// if (hindsa % 3 == 0) {
+//     console.log("Number is divisible by 3");
+
+// }
+
+// else {
+//     console.log("Number is not divisible by 3");
+// }
+
+
+
+// let hindsa1 = 17;
+
+// if (hindsa1 % 2 == 0) {
+//     console.log("Number is Even.");
+
+// }
+
+// else {
+//     console.log("Number is Odd.");
+// }
+
+
+
+// let myAge = +prompt("Enter Your Age");
+
+// if (myAge > 18 && myAge <= 27) {
+//     console.log("You Are Jawan");
+
+//     if (myAge > 27 && myAge <= 40) {
+//         console.log("You Are Mard");
+
+//     } else {
+//         console.log("You Are Buddha");
+//     }
+
+// } else {
+//     console.log("tere bas ka nhi");
+// }
+
+
+
+// let myAge = +prompt("Enter Your Age");
+
+// if (myAge >= 18 && myAge <= 27) {
+//     console.log("Aa gya tu jawan ho k.");
+
+//     if (myAge < 18) {
+//         console.log("Abhi ap choty ho.");
+//     }
+
+// } else {
+//     console.log("Mard Door rahen");
+// }
+
+
+
+// let force = "CID";
+
+// let kon = prompt("Ap kon ho?");
+
+// if (kon == force ) {
+//     console.log("Bhaaaagooo!");
+
+// } else {
+//     console.log("Jao jao apna kam kro");
+// }
+
+
+
+let pehlaNumber = 12;
+let dusraNumber = 23;
+let operator = prompt("Enter Operator");
+
+
+if (operator === "+") {
+    let sum = pehlaNumber + dusraNumber;
+    console.log(sum);
+
+} else if (operator === "-") {
+    let sub = pehlaNumber - dusraNumber;
+    console.log(sub);
+
+} else if (operator === "*") {
+    let multiply = pehlaNumber * dusraNumber;
+    console.log(multiply);
+
+} else if (operator === "/") {
+    let divi = pehlaNumber / dusraNumber;
+    console.log(divi);
+}
+
+else {
+    console.log("Operator sahi dalo");
+}
+
+
+
+
+
+
+
+
+
+
